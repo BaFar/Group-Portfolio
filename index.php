0f61bf2336c9bac1a6d0033123f8226cdc5f6a5a
bafar <bashir.pure@gmail.com>
2017-11-04 22:40:26 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "Welcome start"."<br>";
+$var = 5;
+$str_var = "Hasan" .$var;
+echo $str_var.nl2br("\n");
+
+	if ($var % 2 ==0) {
+		echo "Even Number <br>";
+	}
+	else{
+		echo "Odd Number";
+	}
+?>
