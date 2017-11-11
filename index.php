d65dc880c378a668b2e2fabb77055b62f222014e
bafar <bashir.pure@gmail.com>
2017-11-11 23:05:37 +0600

    initial commit menu bar and 1st carousel completed

--- index.php
+++ index.php
@@ -1,13 +1,120 @@
-<?php
-echo "Welcome start"."<br>";
-$var = 5;
-$str_var = "Hasan" .$var;
-echo $str_var.nl2br("\n");
+<!DOCTYPE html>
+<html>
+<head>
+	<title>banglasoft.com</title>
+	<meta charset="utf-8">
+	<meta name="viewport" width="device-width" initial-scale="1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Js  imported here-->
+ <script src="js/bootstrap.min.js">
+ 	$('.carousel').carousel({
+  interval: 2000
+})
+ </script> 
+ <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+<!-- Js  imported here-->
+</head>
+<body>
+	<div class="page_header">	
+	
+	<div class="nav navbar-inverse nav-position element-color">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="navbar-header">
+						<button class="navbar-toggle" data-target="#mobile_manu" data-toggle="collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a href="#" class="navbar-brand element-color">BanglaSoft</a>
+					
+					</div>
+					<ul class="nav navbar-nav navbar-collapse collapse navbar-right " id="mobile_manu">
+						<li class="active"><a href="#">Feature</a></li>
+						<li><a href="#" class="element-color">Portfolio</a></li>
+						<li><a href="#" class="element-color">About</a></li>
+						<li><a href="#" class="element-color">Team</a></li>
+						<li><a href="#" class="element-color">Pricing</a></li>
+						<li><a href="#" class="element-color">Training</a></li>
+						<li><a href="#" class="element-color">Testimonials</a></li>
+						<li><a href="#" class="element-color">Partner</a></li>
+						<li><a href="#" class="element-color">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 
-	if ($var % 2 ==0) {
-		echo "Even Number <br>";
-	}
-	else{
-		echo "Odd Number";
-	}
-?>
+
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img/grass-flower-morning.jpeg" alt="Nature Picture" style="width: 100%; height: 100%">
+      <div class="carousel-caption">
+        <P>NO Caption</P>
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/natureimage1.jpeg" alt="Nature Picture" style="width: 100%; height: 100%">
+      <div class="carousel-caption">
+        <P>NO Caption</P>
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/natureimage2.jpeg" alt="Nature Picture" style="width: 100%; height: 100%">
+      <div class="carousel-caption">
+        <P>NO Caption</P>
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/natureimage3.jpeg" alt="Nature Picture" style="width: 100%; height: 100%">
+      <div class="carousel-caption">
+        <P>NO Caption</P>
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/natureimage4.jpeg" alt="Nature Picture" style="width: 100%; height: 100%">
+      <div class="carousel-caption">
+        <P>NO Caption</P>
+      </div>
+    </div>
+    <!--some thing here-->
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+</div>
+
+
+<div class="row">
+
+
+
+
+</div>
+
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
