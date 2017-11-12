ad649b9f1fa7b967d30de0c0e83d1e06850afa9e
bafar <bashir.pure@gmail.com>
2017-11-12 23:05:37 +0600

    structure completed upto progress bar

--- index.php
+++ index.php
@@ -113,18 +113,37 @@
 <div class="feature-block">
   <div class="main-feature">
     <div class="container">
-      <div class="col-lg-3 single-feature" ><img src="img/smallimg1.jpeg" alt="small image" class="single-small-img"></div>
-      <div class="col-lg-3 single-feature" ><img src="img/smallimg2.jpeg" alt="small image" class="single-small-img"></div>
-      <div class="col-lg-3 single-feature" ><img src="img/smallimg3.jpeg" alt="small image" class="single-small-img"></div>
-      <div class="col-lg-3 single-feature" ><img src="img/smallimg4.jpeg" alt="small image" class="single-small-img"></div>
+      <div class="row">
+
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xm-12 single-feature" >
+          <img src="img/smallimg1.jpeg" alt="small image" class="img-responsive center-block single-small-img">
+        </div>
+
+      <div class="col-lg-3 ol-md-3 col-sm-6 col-xm-12  single-feature" >
+        <img src="img/smallimg2.jpeg" alt="small image" class="img-responsive center-block single-small-img">
+      </div>
+
+      <div class="col-lg-3 ol-md-3 col-sm-6 col-xm-12  single-feature" >
+        <img src="img/smallimg3.jpeg" alt="small image" class="img-responsive center-block single-small-img">
+      </div>
+
+      <div class="col-lg-3 ol-md-3 col-sm-6 col-xm-12 single-feature" >
+        <img src="img/smallimg4.jpeg" alt="small image" class="img-responsive center-block single-small-img">
+      </div>
+
+      </div>      
     </div>
   </div>
+
+
   <div class="feature-text">
     <div class="container">
-      <div>
+      <div class="row">
+        <div class="col-md-12">
         <h4>FindingSoft assembled some topnotch software engineers in our country to work together.
 Each member of the team has done something exceptional in different areas.</h4>
-        <button class="btn btn-primary btn-lg" ><a href="#" style="color: #fff">Banglasoft</a></button>
+        <button type="submit" class="btn btn-primary" ><a href="#" style="color: #fff">Banglasoft</a></button>
+      </div>
       </div>
     </div>
   </div>
@@ -141,8 +160,114 @@ Each member of the team has done something exceptional in different areas.</h4>
         <div class="col-lg-4 single-portfolio"><img src="img/webdev.jpeg" alt="web"></div>
         <div class="col-lg-4 single-portfolio"><img src="img/mobiledev.jpeg" alt="mobile"></div>
       </div>
+    </div>    
+  </div>
+  <!--End of Portfolio block-->
+
+  <!--Start of about us block-->
+  <div class="about-us-section1">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-12 col-xs-12">
+              <div class="about_us-header text-center">
+                  <h4>About Us</h4>
+                  <p>Journey Towards A Dream Roaed</p>
+              </div>
+          </div>
+       </div>
+
+       <div class="row">
+          <div class="about-us-description ">
+
+            <div class="single-thought col-md-4 col-sm-6 col-xs-12 text-center" >
+              <img src="img/aboutimg1.jpeg" alt="about" class="img-responsive center-block">
+              <h4>Office philoshopy</h4>
+                  <div class="border"></div>
+              <p>Our resource pool is constructed with many diverse IT professionals with strong technology skills and business knowledge. Our mature methodologies and cost-efficient delivery model enable us to effectively handle software projects of any scale and complexity.</p>
+            </div>
+
+            <div class="single-thought col-md-4 col-sm-6 col-xs-12 text-center" >
+              <img src="img/aboutimg2.jpeg" alt="about" class="img-responsive center-block">
+              <h4>Office Vision</h4>
+                  <div class="border"></div>
+              <p>As pioneers in computation and computational knowledge, we have pursued a long-term vision to develop the science, technology, and tools to make computation an ever-more-potent force in today's and tomorrow's world.</p>
+            </div>
+
+            <div class="single-thought col-md-4 col-sm-6 col-xs-12 text-center" >
+              <img src="img/aboutimg3.jpeg" alt="about" class="img-responsive center-block">
+              <h4>Office Values</h4>
+              <div class="border"></div>
+              <p> • We care about the quality of our products<br>
+                  • We care about our customers<br>
+                  • We care about our employees<br>
+                  • We are committed to doing our best every time. </p>
+            </div>
+          </div>
+        </div>          
+      </div>
+  </div>
+
+  <div class="about-us-section2">
+    <div class="container">
+      <div class="row">
+        
+          <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+            <div class="row">
+
+              <div class="skill">
+                <p>Web Design</p>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="60" style="width: 90%;">
+                    <span class="progress-bar-span">90%</span>                   
+                  </div>
+                </div>
+              </div>
+
+              <div class="skill">
+                <p>HTML & CSS</p>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="60" style="width: 95%;">
+                    <span class="progress-bar-span">95%</span>                   
+                  </div>
+                </div>
+              </div>
+
+              <div class="skill">
+                <p>WordPress</p>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="60" style="width: 80%;">
+                    <span class="progress-bar-span">80%</span>                   
+                  </div>
+                </div>
+              </div>
+
+              <div class="skill">
+                <p>Joomla</p>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="60" style="width: 60%;">
+                    <span class="progress-bar-span">60%</span>                   
+                  </div>
+                </div>
+              </div>
+
+              <div class="skill">
+                <p>Extension</p>
+                <div class="progress">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="60" style="width: 70%;">
+                    <span class="progress-bar-span">70%</span>                   
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+            
+          </div>
+           
+      </div>
     </div>
-    
   </div>
 
 <script type="text/javascript" src="js/bootstrap.js"></script>
